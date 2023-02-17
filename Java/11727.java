b30b2f79af86c37e927b85c3813f80e56ebf623b
dongmin <ehdals45454@naver.com>
2023-02-17 19:33:51 +0900

    2xn 타일링2

---
+++ Java/11727.java
@@ -0,0 +1,24 @@
+package Silver.n11727;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+
+        int[] result = new int[1001];
+
+        result[0] = 0;
+        result[1] = 1;
+        result[2] = 3;
+
+        for (int i = 3; i <= n; i++) {
+            result[i] = (result[i - 1] + result[i - 2] * 2) % 10007;
+        }
+        System.out.println(result[n]);
+
+
+    }
+}
