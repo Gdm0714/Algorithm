fd8824609695dfda27ad1c5e19b2e4c57ddd27ab
Dongmin <50660440+Gdm0714@users.noreply.github.com>
2024-04-02 14:30:33 +0900

    [Gold V] Title: LCS, Time: 160 ms, Memory: 18528 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+
+        int aLen = a.length();
+        int bLen = b.length();
+
+        int[][] dp = new int[aLen + 1][bLen + 1];
+
+        for (int i = 1; i <= aLen; i++) {
+            for (int j = 1; j <= bLen; j++) {
+                if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        System.out.println(dp[aLen][bLen]);
+    }
+}
