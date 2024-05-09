db02ccafa714309f86e2d934a59dec3aab5400b7
Dongmin <ehdals45454@naver.com>
2024-05-09 13:38:39 +0900

    [Gold IV] Title: 수도배관공사, Time: 196 ms, Memory: 15592 KB -BaekjoonHub

---
+++ 백준/Gold/2073. 수도배관공사/수도배관공사.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int d = Integer.parseInt(st.nextToken());
+        int p = Integer.parseInt(st.nextToken());
+        
+        int dp[] = new int[d + 1];
+        dp[0] = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < p; i++) {
+            st = new StringTokenizer(br.readLine());
+            
+            int l = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            
+            for (int j = d; j >= l; j--) dp[j] = Math.max(dp[j], Math.min(dp[j - l], c));
+            
+        }
+        System.out.println(dp[d]);
+    }
+}
