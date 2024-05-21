abcf09fe21ad9faf182144876154d93ffa9e1795
Dongmin <ehdals45454@naver.com>
2024-05-21 17:06:23 +0900

    [Silver V] Title: 다리 놓기, Time: 156 ms, Memory: 14556 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static long[][] dp = new long[30][30];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        long[][] dp = new long[30][30];
+        for (int i = 0; i < 30; i++) {
+            dp[i][0] = dp[i][i] = 1;
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+        }
+
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken()); 
+            int m = Integer.parseInt(st.nextToken());
+            System.out.println(dp[m][n]);
+        }
+    }
+}
