7a17ae6220deea3ee874cb87e8f8068d528d10c4
Dongmin <ehdals45454@naver.com>
2024-06-30 03:43:24 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 668 ms, Memory: 60700 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        HashMap<String, String> pw = new HashMap<>();
+
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            pw.put(st.nextToken(), st.nextToken());
+        }
+        for (int i = 0; i < m; i++) {
+            bw.write(pw.get(br.readLine()) + "\n");
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
