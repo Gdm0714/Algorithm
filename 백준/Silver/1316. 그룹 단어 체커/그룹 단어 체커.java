f59de5ce11efeb63036394c0f9cf8c94d908c4b5
Dongmin <ehdals45454@naver.com>
2024-10-29 23:37:58 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 220 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+
+        int result = 0;
+
+        for (int i = 0; i < num; i++) {
+            String text = s.next();
+            int cnt = 0;
+            for (int j = 0; j < text.length() - 1; j++) {
+                if (text.charAt(j) != text.charAt(j + 1)) {
+                    for (int k = j + 2; k < text.length(); k++) {
+                        if (text.charAt(j) == text.charAt(k)) {
+                            cnt++;
+                        }
+                    }
+                }
+            }
+            if (cnt == 0) {
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+}
