9385e01fa237244cad1876adfe9fa675453d5e2f
Dongmin <ehdals45454@naver.com>
2025-01-11 17:51:49 +0900

    [Gold IV] Title: 부분합, Time: 248 ms, Memory: 23084 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,35 +7,34 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int s = Integer.parseInt(st.nextToken());
+
         int[] arr = new int[n];
+        int min = Integer.MAX_VALUE;
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        int min = Integer.MAX_VALUE;
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            int start = i;
-            sum = 0;
-            sum += arr[start];
-            if(sum >= s) {
-                min = Math.min(min, 1);
-                break;
-            }
-            for (int j = start + 1; j < n; j++) {
-                sum += arr[j];
-                if (sum >= s) {
-                    min = Math.min(min, j - start + 1);
-                    break;
-                }
+
+        while (true) {
+            if (sum < s) {
+                if (end >= n) break;
+                sum += arr[end];
+                end++;
+            } else {
+                min = Math.min(min, end - start);
+                sum -= arr[start];
+                start++;
             }
         }
-        if (min == Integer.MAX_VALUE) {
-            System.out.println(0);
-        } else {
-            System.out.println(min);
-        }
+
+        if (min == Integer.MAX_VALUE) System.out.println(0);
+        else System.out.println(min);
     }
 }
