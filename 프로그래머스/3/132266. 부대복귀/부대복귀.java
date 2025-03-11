5486a34cd90a4628d3a6de9d9f4a351eae946006
Dongmin <ehdals45454@naver.com>
2025-03-12 01:10:00 +0900

    [level 3] Title: 부대복귀, Time: 170.65 ms, Memory: 202 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -2,29 +2,27 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
-        int[] answer = new int[sources.length];
         ArrayList<ArrayList<Integer>> lists = new ArrayList<>();
-        for(int i = 0; i <= n; i++){
+        
+        for(int i = 0; i < n + 1; i++){
             lists.add(new ArrayList<>());
         }
         
-        for(int[] i: roads){
+        for(int i[]: roads){
             lists.get(i[0]).add(i[1]);
             lists.get(i[1]).add(i[0]);
         }
         
         int[] d = new int[n + 1];
-        
         Arrays.fill(d, -1);
-        
+        d[destination] = 0;
         Queue<Integer> q = new LinkedList<>();
         q.add(destination);
-        d[destination] = 0;
         
         while(!q.isEmpty()){
             int cur = q.poll();
             
-            for(int i : lists.get(cur)){
+            for(int i: lists.get(cur)){
                 if(d[i] == -1){
                     d[i] = d[cur] + 1;
                     q.add(i);
@@ -32,6 +30,7 @@ class Solution {
             }
         }
         
+        int[] answer = new int[sources.length];
         for(int i = 0; i < sources.length; i++){
             answer[i] = d[sources[i]];
         }
