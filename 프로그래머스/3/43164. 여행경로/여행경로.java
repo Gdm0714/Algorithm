99d281e2cc58352bba1fb970ac6004e94e50a1d1
Dongmin <ehdals45454@naver.com>
2025-03-19 21:29:00 +0900

    [level 3] Title: 여행경로, Time: 130.39 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,26 +2,26 @@ import java.util.*;
 
 class Solution {
     boolean[] visited;
-    ArrayList<String> list;
+    ArrayList<String> answer;
     public String[] solution(String[][] tickets) {
-        list = new ArrayList<>();
         visited = new boolean[tickets.length];
-        dfs("ICN", tickets, "ICN", 0);
-        Collections.sort(list);
+        answer = new ArrayList<>();
+        dfs("ICN", "ICN", tickets, 0);
+        Collections.sort(answer);
         
-        return list.get(0).split(" ");
+        return answer.get(0).split(" ");
     }
     
-    void dfs(String start, String[][] tickets, String end, int cnt){
+    void dfs(String start, String end, String[][] tickets, int cnt){
         if(cnt == tickets.length){
-            list.add(end);
+            answer.add(end);
             return;
         }
         
         for(int i = 0; i < tickets.length; i++){
             if(!visited[i] && tickets[i][0].equals(start)){
                 visited[i] = true;
-                dfs(tickets[i][1], tickets, end + " " + tickets[i][1], cnt + 1);
+                dfs(tickets[i][1], end + " " + tickets[i][1], tickets, cnt + 1);
                 visited[i] = false;
             }
         }
