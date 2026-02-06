c87497ac7f1e8d44c6f4548aaf71e8810adb5156
Dongmin <ehdals45454@naver.com>
2026-02-06 22:38:53 +0900

    [level 2] Title: 더 맵게, Time: 226.51 ms, Memory: 40.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    priority_queue<int, vector<int>, greater<int>> pq(scoville.begin(), scoville.end());
+    int answer = 0;
+    
+    while (pq.size() >= 2 && pq.top() < K) {
+        int first = pq.top(); pq.pop();
+        int second = pq.top(); pq.pop();
+        pq.push(first + second * 2);
+        answer++;
+    }
+    
+    if (pq.top() < K) return -1;
+    return answer;
+}
